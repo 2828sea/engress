e99e7a5f76a8d55f0a86e91909bebd49e9d2fb75
2828sea <2828seaaa@gmail.com>
2022-03-13 17:31:26 +0900

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  var topBtn = $('.pagetop');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  //ドロワーメニュー
+  $("#MenuButton").click(function () {
+    // $(".l-drawer-menu").toggleClass("is-show");
+    // $(".p-drawer-menu").toggleClass("is-show");
+    $(".js-drawer-open").toggleClass("open");
+    $(".drawer-menu").toggleClass("open");
+    $("html").toggleClass("is-fixed");
+
+  });
+
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('js-header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+
+  //ヘッダー分の高さコンテンツを下げる
+  jQuery(function ($) {
+    var height = jQuery('.l-header').height()
+    jQuery('body').css('margin-top', height)
+  })
+
+
+// **********************************
+//  ハンバーガーメニュー
+// **********************************
+
+
+  $('.js-hamburger').click(function () {
+    $(this).toggleClass('active')
+
+    if ($(this).hasClass('active')) {
+      $('.js-globalMenuSp').addClass('active')
+    } else {
+      $('.js-globalMenuSp').removeClass('active')
+    }
+  })
+
+});
